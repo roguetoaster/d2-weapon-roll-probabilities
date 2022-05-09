4b82a85b55276ca574468d029d77402183412f95
roguetoaster <105230129+roguetoaster@users.noreply.github.com>
2022-05-09 13:33:48 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+var apiKey = "73253fe956a4446fb27bf648c456cfcb";
 
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.setRequestHeader("X-API-Key", apiKey);
+
+xhr.onreadystatechange = function(){
+ if(this.readyState === 4 && this.status === 200){
+  var json = JSON.parse(this.responseText);
+  console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+ }
+}
+
+xhr.send();
